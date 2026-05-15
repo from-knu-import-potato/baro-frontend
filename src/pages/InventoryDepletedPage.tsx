4c2134c464b5d71c348cfedbf53d220a4102032a
daaoooy <y020d226y@daum.net>
2026-05-15 23:39:57 +0900

    ✨ (#25) feat: 임시 재고 및 가격 분석 페이지 추가

---
+++ src/pages/InventoryDepletedPage.tsx
@@ -0,0 +1,9 @@
+const InventoryDepletedPage = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-xl font-bold text-baro-black">소진 재고 현황 (임시)</h1>
+    </div>
+  );
+};
+
+export default InventoryDepletedPage;
