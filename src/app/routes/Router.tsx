b129dfc8601019331462bee5efb093bde5043207
daaoooy <y020d226y@daum.net>
2026-05-15 18:05:04 +0900

    🔥 (#21) remove: router 경로에서 회원가입 페이지 삭제

--- src/app/routes/Router.tsx
+++ src/app/routes/Router.tsx
@@ -4,14 +4,12 @@ import { routePaths } from '@/app/routes/routePaths';
 import LandingPage from '@/pages/LandingPage';
 import LoginPage from '@/pages/LoginPage';
 import NotFoundPage from '@/pages/NotFoundPage';
-import SignupPage from '@/pages/SignupPage';
 
 export default function Router() {
   return (
     <Routes>
       <Route path={routePaths.landing} element={<LandingPage />} />
       <Route path={routePaths.login} element={<LoginPage />} />
-      <Route path={routePaths.signup} element={<SignupPage />} />
       <Route path={routePaths.notFound} element={<NotFoundPage />} />
     </Routes>
   );
--- src/app/routes/routePaths.ts
+++ src/app/routes/routePaths.ts
@@ -1,6 +1,5 @@
 export const routePaths = {
   landing: '/',
   login: '/login',
-  signup: '/signup',
   notFound: '*',
 } as const;
