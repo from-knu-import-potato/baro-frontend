f48d89702719050787432a867cd4ca6a49d9f912
daaoooy <y020d226y@daum.net>
2026-05-29 01:51:47 +0900

    🔧 chore: App 컴포넌트에 Toaster 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from 'react-router-dom';
 
 import Router from '@/app/routes/Router';
+import { Toaster } from '@/shadcn/ui/sonner';
 
 function App() {
   return (
     <BrowserRouter>
       <Router />
+      <Toaster position="top-center" />
     </BrowserRouter>
   );
 }
