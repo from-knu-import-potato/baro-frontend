b218b1b6e9dd3279944fc0516960548eefcadeba
daaoooy <y020d226y@daum.net>
2026-06-05 18:17:14 +0900

    ✨ feat: 앱 초기화 로직, QueryClientProvider 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
 
+import AppInitializer from '@/app/AppInitializer';
 import Router from '@/app/routes/Router';
 import { Toaster } from '@/shadcn/ui/sonner';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <BrowserRouter>
-      <Router />
-      <Toaster position="top-center" />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <AppInitializer>
+          <Router />
+          <Toaster position="top-center" />
+        </AppInitializer>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
