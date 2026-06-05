aa30e26b49937ed97925f855744db3b55ce92b11
daaoooy <y020d226y@daum.net>
2026-06-05 19:05:07 +0900

    ♻️ refactor: refreshToken 처리 로직 및 storeId 설정 추가

--- src/app/AppInitializer.tsx
+++ src/app/AppInitializer.tsx
@@ -6,20 +6,35 @@ import axiosInstance from '@/shared/api/axiosInstance';
 const AppInitializer = ({ children }: { children: React.ReactNode }) => {
   const [ready, setReady] = useState(false);
   const setAccessToken = useAuthStore((s) => s.setAccessToken);
+  const setStoreId = useAuthStore((s) => s.setStoreId);
   const clearAuth = useAuthStore((s) => s.clearAuth);
 
   useEffect(() => {
-    const refreshToken = useAuthStore.getState().refreshToken;
+    const { refreshToken } = useAuthStore.getState();
 
-    const init = refreshToken
+    const tokenInit = refreshToken
       ? axiosInstance
           .post('/auth/refresh', { refreshToken })
           .then((res) => setAccessToken(res.data.data.accessToken))
           .catch(() => clearAuth())
       : Promise.resolve();
 
+    const init = tokenInit.then(() => {
+      const { accessToken, storeId } = useAuthStore.getState();
+      if (accessToken && !storeId) {
+        return axiosInstance
+          .get('/users/me/store')
+          .then((res) => {
+            if (res.data.data?.storeId) {
+              setStoreId(res.data.data.storeId);
+            }
+          })
+          .catch(() => {});
+      }
+    });
+
     init.finally(() => setReady(true));
-  }, [setAccessToken, clearAuth]);
+  }, [setAccessToken, setStoreId, clearAuth]);
 
   if (!ready) return null;
 
